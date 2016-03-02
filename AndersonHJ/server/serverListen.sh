844f9693a118922df19b4cdbdcb54ce882e3dd23
Jian Hou <houjian.max@gmail.com>
2016-03-01 22:44:23 -0800

    Create serverListen.sh

---
+++ AndersonHJ/server/serverListen.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#The pid of the server program is 18631
+while true;
+do
+#Execute the command to find the number of open connections to the server.
+sudo netstat -anp|grep 18631|wc -l;
+#Execute the command to find the memory consumption of the server while processing the load.
+sudo ps aux|grep 18631|awk '{print$4,$5}';
+sleep 5;
+done
