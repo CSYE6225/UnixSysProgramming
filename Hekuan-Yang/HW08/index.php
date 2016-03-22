202791af70fd62bbd42067b56adaf9a216169d93
Hekuan Yang <Exodus000@users.noreply.github.com>
2016-03-22 16:35:18 -0700

    Create index.php

---
+++ Hekuan-Yang/HW08/index.php
@@ -0,0 +1,15 @@
+<?php
+// Allocate memory per the VM capacity
+ini_set('memory_limit', '-1');
+// Get a file into an array.  In this example we'll go through HTTP to get
+// the HTML source of a URL.
+$lines = file('1.log');
+
+// Loop through our array, show HTML source as HTML source; and line numbers too.
+sort($lines);
+/*
+foreach ($lines as $line_num => $line) {
+    echo "Line {$line_num} : $line\n";
+}*/
+echo "Done";
+?>
