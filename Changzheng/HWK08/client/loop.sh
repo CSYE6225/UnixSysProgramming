6adf2808d9086fd32751ee5283711f8090902188
mchzh <mchzh_qlj@hotmail.com>
2016-03-27 23:43:40 -0700

    Added files via upload

---
+++ Changzheng/HWK08/client/loop.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+while true
+do
+#curl http://54.187.15.159/big_buck_bunny_480p_20mb.mp4
+curl http://54.191.99.127/load.php
+sleep 0.0001
+done
---
+++ Changzheng/HWK08/client/loop50mb.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+while true
+do
+curl http://54.187.15.159/50mb.mp4
+sleep 0.0001
+done
