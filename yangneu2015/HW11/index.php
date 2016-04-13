36b89b7a0a8d3e7a8a7eba6a40c756c94e1f5bf5
yangneu2015 <liu.yang14@husky.neu.edu>
2016-04-12 19:10:12 -0700

    Create index.php

---
+++ yangneu2015/HW11/index.php
@@ -0,0 +1,11 @@
+<?php
+    //get the directory list of /proc
+    $arr = scandir("/proc/");
+    //loop to srot the array $arr ascendingly and descendly
+    for($x=0;$x<10000;$x++){
+        asort($arr);
+        arsort($arr);
+    }
+    print_r($arr);
+    echo "server1";
+?>
